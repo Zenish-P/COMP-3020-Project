b7379a20cdedd0b9843f19ff3fafa745cf44310f
jinmichael_98 <jinmichael_98@live.com>
2022-11-27 19:33:51 -0600

    Add music player script

--- Js/script.js
+++ Js/script.js
@@ -57,5 +57,28 @@ function switchPage(page) {
                 $(buttons[b]).removeClass('active');
             }
         }
+        
+        if (page === 'music-player') {
+          function getUrlParameter(sParam) 
+          {
+            var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+            for (i = 0; i < sURLVariables.length; i++) 
+            {
+              sParameterName = sURLVariables[i].split('=');
+
+              if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+              }
+            }
+            return false;
+          };
+
+          $('#musicName').text(getUrlParameter('musicname') + ' - ' + getUrlParameter('bandname'));
+          $('#musicTime').text(getUrlParameter('time'));
+        }
     }
 }
